2e16eb55b1e6dc818027ff61ced26e1587b70248
Theonlyabiluv <opuoruabigailelohor@gmail.com>
2025-04-09 23:47:32 -0700

    Higher order function assignment

---
+++ Desktop/Week10/Assignment/app.js
@@ -0,0 +1,72 @@
+// Higher Order Functions Assignment
+// Functions Assignment 
+def fullName_print('Abigail, Opuoru') ;
+ "Prints out my full name.";
+ print("Abigail, Opuoru")
+//Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+print(fullName_print('Abigail,  Opuoru'))
+
+ def fullName_return(firstName, lastName);
+"Return my fullName"
+return firstName + " " + lastName
+print(fullName_return('Abigail', 'Opuoru'))
+
+// Declare a function addNumbers and it takes two two parameters and it returns sum.
+addNumbers(num1, num2);
+"Return the sum of two numbers"
+return num1 + num2
+print(addNumbers(5, 10))
+print(sum_of_numbers(5, 10))
+
+// Higher Order Functions Assignment
+const countries = [
+    'Albania', 'Bolivia', 'Canada', 'Denmark', 'Ethiopia',
+    'Finland', 'Germany', 'Hungary', 'IceLand', 'Ireland',
+    'Japan', 'Kenya',
+  ];
+  const names = ['Tamara', 'Mathias', 'Elias', 'Brook'];
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const products = [
+    { product: 'banana', price: 3 },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: 8 },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+  ];
+  countries.forEach(Albania => console.log('Albania'));
+  countries.forEach(Bolivia => console,log('Bolivia'));
+  countries,forEach(Canada => console.log('canada'));
+  countries,forEach(Denmark => console.log('Denmark'));
+  countries,forEach(Ethiopia => console.log('Ethiopia'));
+  countries,forEach(Finland => console.log('Finland'));
+  countries,forEach(Germany => console.log('Germany'));
+  countries,forEach(Hungary => console.log('Hungary'));
+  countries,forEach(Iceland => console.log('Iceland'));
+  countries,forEach(Ireland => console.log('Ireland'));
+  countries,forEach(Japan => console.log('Japan'));
+  countries,forEach(Kenya => console.log('Kenya'));
+
+  names.forEach(Tamara => console.log('Tamara'));
+  names.forEach(Mathias => console.log('Mathias'));
+  names.forEach(Elias => console.log('Elias'));
+  names.forEach(Brook => console.log('Brook'));
+
+  numbers.map(1 => console.log(1));
+  numbers.map(2 => console.log(2));
+  numbers.map(3 => console.log(3));
+  numbers.map(4 => console.log(4));
+  numbers.map(5 => console.log(5));
+  numbers.map(6 => console.log(6));
+  numbers.map(7 => console.log(7));
+  numbers.map(8 => console.log(8));
+  numbers.map(9 => console.log(9));
+  numbers.map(10 => console.log(10));
+
+  const upperCountries = countries.map(Albania => country.toUpperCase('ALBANIA'));
+console.log(upperCountries);
+const upperCountries = countries.map(Bolivia => country.toUpperCase('BOLIVIA'));
+console.log('bolivia') 'BOLIVIA';
+
+
